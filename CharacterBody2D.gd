e89efb85c295e0de8120113b90a6b4569fc8ba5a
raspberrypandad <davidepmwork@gmail.com>
2024-07-19 12:53:32 +0100

    new main scene

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -17,8 +17,7 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement/deceleration..
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
