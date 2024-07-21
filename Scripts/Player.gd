4fdd57b56ebc774c23e49b8ad6bcb4eaaced1bde
Taylor Woods <taywoods6284@gmail.com>
2024-07-21 09:04:47 -0400

    Updated Scripts with comments and added the abilty to wall jump after moving off a wall (like a coyote wall jump) and A BUNCH of conditonal statements to make it work.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,19 +1,30 @@
 extends CharacterBody2D
-
+#
+# DO NOT CHANGE A VAR TO A CONST UNLESS A COMMENT SAYS OTHERWISE
+#
 @onready var _animated_sprite = $AnimatedSprite2D
-var move_speed = 300.0
-var jump_force = -350.0  
-var bottom_bound = 150
-var jump_count = 0
-var max_jumps = 2
-var gravity = 20
+
+const move_speed : float = 200 # Variable that acts as constant so when player_speed is adjusted, the base move_speed still exists
+var player_speed : float # Variable that represents players current speed
+
+const gravity : float = 15 # The gravity var
+
+var jump_force : float = -300  # How powerful the jump is (could be a const)
+var bottom_bound : int = 150 # How many pixels down the death barrier is
+
+var jump_count : int = 0 # How many time the player has jumped 
+var max_jumps : int = 2 # The max amount of time's the player can jump
+
+#-- Used for preventing people jumping on the same wall they just jumped off of --#
+var did_left_jump : bool = false
+var did_right_jump : bool = false
 
 
 func _physics_process(delta):
 	movement_control(delta)
 	move_animation()
 	
-func game_over(): #Reloads scene when death
+func game_over(): # Reloads scene when death
 	get_tree().reload_current_scene()
 	
 func move_animation(): # Hold animations for the movement
@@ -25,22 +36,42 @@ func move_animation(): # Hold animations for the movement
 		_animated_sprite.stop()
 
 func movement_control(delta): # Holds all movement control
-	if not is_on_floor(): #Applies gravity if not in floor
+	velocity.x = 0 # Set's to zero so it isn't constantly moving
+	
+	if not is_on_floor() and not is_on_wall(): # Applies gravity if not in floor and not on a wall
 		velocity.y += gravity
-	if is_on_floor():
-		jump_count = 0
 		
-	velocity.x = 0 #Set's to zero so it isn't constantly moving
+	if is_on_wall():
+		jump_count = 0 # Resets Amount of times jumped
+		max_jumps = 1 # Sets jump max to 1
+		did_left_jump = false # Resets directional jump checker
+		did_right_jump = false # Resets directional jump checker
+		player_speed = move_speed * .70 # Lowers players speed untill they touch the floor again
+		velocity = Vector2.ZERO # Stops player's momentum when colliding with a wall
+		velocity.y += gravity * 0.4 # Slows gravity's effect on the player when on a wall
+		
+		#-- If statement for preventing the player from changing direction mid-air after wall jump--#
+		if Input.is_action_just_pressed("left")  :
+			did_left_jump = true
+		elif Input.is_action_just_pressed("right") :
+			did_right_jump = true
+			
+	if is_on_floor():
+		max_jumps = 2 # Sets jump max to 2
+		did_left_jump = false # Resets directional jump checker
+		did_right_jump = false # Resets directional jump checker
+		player_speed = move_speed #Reset player speed to base speed on floor
+		jump_count = 0 # Resets Amount of times jumped
+		
+	if Input.is_key_pressed(KEY_LEFT) and !did_right_jump: # Move left aslong as they didn't just jump right off a wall
+		velocity.x -= player_speed # Moves the player left
+	if Input.is_key_pressed(KEY_RIGHT) and !did_left_jump: # Move right aslong as they didn't just jump left off a wall
+		velocity.x += player_speed # Moves the player right
 	
-	if Input.is_key_pressed(KEY_LEFT): #Velocity left for moving left
-		velocity.x -= move_speed 
-	if Input.is_key_pressed(KEY_RIGHT):#Velocity right for moving right
-		velocity.x += move_speed
-	
-	if jump_count < max_jumps: 
-		if Input.is_action_just_pressed("ui_accept") : #Velocity for the jump
-			velocity.y = jump_force
-			jump_count+=1
+	if jump_count < max_jumps: #Checks if the player has jumped the max amount of time or not
+		if Input.is_action_just_pressed("ui_accept") and !is_on_wall() : #Checks if space was just pressed and not on a wall.
+			velocity.y = jump_force #Moves player up
+			jump_count+=1 #Increments jump count by 1 when you have jumped
 		
 	if global_position.y > bottom_bound: #Game over if below bottom bound
 		game_over()
