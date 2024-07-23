ca4a44effb689e5672faf7b11cb1af385abe7907
Taylor Woods <taywoods6284@gmail.com>
2024-07-23 05:36:13 -0400

    Adjusted the rays it made it possible in the script so that a person can't infintly jump on a single wall.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -15,9 +15,11 @@ var bottom_bound : int = 150 # How many pixels down the death barrier is
 var jump_count : int = 0 # How many time the player has jumped 
 var max_jumps : int = 2 # The max amount of time's the player can jump
 
-#-- Used for preventing people jumping on the same wall they just jumped off of --#
-var did_left_jump : bool = false
-var did_right_jump : bool = false
+@onready var left_ray = $RayCast_Left
+@onready var right_ray = $RayCast_Right
+var did_left_jump = false
+var did_right_jump = false
+
 
 const dash_speed = 900
 var is_dashing = false
@@ -49,25 +51,24 @@ func movement_control(delta): # Holds all movement control
 		velocity.y += gravity
 		$AnimatedSprite2D.play("fall")
 	
-	if is_on_wall():
-		jump_count = 0 # Resets Amount of times jumped
+	if left_ray.is_colliding() or right_ray.is_colliding():
 		max_jumps = 1 # Sets jump max to 1
-		did_left_jump = false # Resets directional jump checker
-		did_right_jump = false # Resets directional jump checker
 		player_speed = move_speed * .70 # Lowers players speed untill they touch the floor again
 		velocity = Vector2.ZERO # Stops player's momentum when colliding with a wall
 		velocity.y += gravity * 0.4 # Slows gravity's effect on the player when on a wall
-		
-		#-- If statement for preventing the player from changing direction mid-air after wall jump--#
-		if Input.is_action_just_pressed("left")  :
-			did_left_jump = true
-		elif Input.is_action_just_pressed("right") :
-			did_right_jump = true
+	if left_ray.is_colliding() and !did_right_jump:
+		jump_count = 0
+		did_right_jump = true
+		did_left_jump = false
+	if right_ray.is_colliding() and !did_left_jump:
+		jump_count = 0
+		did_left_jump = true
+		did_right_jump = false
 			
 	if is_on_floor():
+		did_left_jump = false
+		did_right_jump = false
 		max_jumps = 2 # Sets jump max to 2
-		did_left_jump = false # Resets directional jump checker
-		did_right_jump = false # Resets directional jump checker
 		player_speed = move_speed #Reset player speed to base speed on floor
 		jump_count = 0 # Resets Amount of times jumped
 	
@@ -77,14 +78,14 @@ func movement_control(delta): # Holds all movement control
 		$dash_timer.start()
 		$can_dash_timer.start()
 	
-	if Input.is_key_pressed(KEY_LEFT) and !did_right_jump: # Move left aslong as they didn't just jump right off a wall
+	if Input.is_key_pressed(KEY_LEFT): # Move left aslong as they didn't just jump right off a wall
 		if is_dashing:
 			velocity.x = direction * dash_speed
 		else: 
 			velocity.x = direction * player_speed # Moves the player left
 		if is_on_floor:
 			$AnimatedSprite2D.play("run")
-	elif Input.is_key_pressed(KEY_RIGHT) and !did_left_jump: # Move right aslong as they didn't just jump left off a wall
+	elif Input.is_key_pressed(KEY_RIGHT): # Move right aslong as they didn't just jump left off a wall
 		if is_dashing:
 			velocity.x = direction * dash_speed
 
@@ -96,7 +97,7 @@ func movement_control(delta): # Holds all movement control
 		velocity.x = 0
 	
 	if jump_count < max_jumps: #Checks if the player has jumped the max amount of time or not
-		if Input.is_action_just_pressed("ui_accept") and !is_on_wall() : #Checks if space was just pressed and not on a wall.
+		if Input.is_action_just_pressed("ui_accept"): #Checks if space was just pressed and not on a wall.
 			velocity.y = jump_force #Moves player up
 			jump_count+=1 #Increments jump count by 1 when you have jumped
 			$AnimatedSprite2D.play("jump")
