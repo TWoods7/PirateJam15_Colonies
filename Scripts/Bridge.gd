8557ed0f77111d4c73aaa9b0f59be74ba9a3f5cd
Taylor Woods <taywoods6284@gmail.com>
2024-07-23 15:30:00 -0400

    Cleaned up scripts and Commented ALOT

--- Scripts/Bridge.gd
+++ Scripts/Bridge.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-@onready var bridge = get_node("CollisionShape2D")
-var state : bool = false
+@onready var bridge = get_node("CollisionShape2D") # Assigns the var as the Collision Child Node
+var state = false # Used for flipping the bridges collision
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+
 func _process(delta):
 	pass
-
-func _on_visibility_changed():
-	state = !state
-	bridge.disabled = !state
+	
+#-- Flips the Collider to disabled or !disabled based on its visibility change --#
+func _on_visibility_changed(): 
+	state = !state # Flips state to either true or false
+	bridge.disabled = !state #Disables the Colliders Collision
+#--------------------------------------------------------------------------------#
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -15,29 +15,102 @@ var bottom_bound : int = 150 # How many pixels down the death barrier is
 var jump_count : int = 0 # How many time the player has jumped 
 var max_jumps : int = 2 # The max amount of time's the player can jump
 
-@onready var left_ray = $RayCast_Left
-@onready var right_ray = $RayCast_Right
-var did_left_jump = false
-var did_right_jump = false
+@onready var left_ray = $RayCast_Left #The ray that tells if something is on the players left
+@onready var right_ray = $RayCast_Right #The ray that tells if something is on the players right
+var did_left_jump = false # Check for if player just jumped to the left
+var did_right_jump = false # Check for if player just jumped to the right
  
-@onready var nodes = get_tree().get_nodes_in_group("Node")
-@onready var bridges = get_tree().get_nodes_in_group("Bridge")
-@onready var index = 0
-@onready var state = false
+@onready var nodes = get_tree().get_nodes_in_group("Node") # The array of nodes that are in the group Node
+@onready var bridges = get_tree().get_nodes_in_group("Bridge") # The array of nodes that are in the group Bridge
+var index = 0 # Index for accessing different set of bridges and their nodes
+var state = false # Used for flipping the bridges visibilty and collision
 
-const dash_speed = 900
-var is_dashing = false
-var can_dash = true
+const dash_speed = 900 #Speed for when your dashing
+var is_dashing = false #check for if the player is currently dashing
+var can_dash = true #Check for if cooldown is over and the player can dash
 
 # func that's called every frame
 func _physics_process(delta):
 	movement_control(delta)
 	move_animation()
+	#-- Breaks level into seperate areas where the plant bridge you change is decided--#
 	if global_position.x > -350 and global_position.x< 250:
 		index = 0
 	if global_position.x > 250:
 		index = 1
+	#----------------------------------------------------------------------------------#
+
+func movement_control(delta): # Holds all movement control
+	var direction = Input.get_axis("left", "right")
 	
+	if (not is_on_floor() and not is_on_wall()): # Applies gravity if not in floor and not on a wall
+		velocity.y += gravity
+		$AnimatedSprite2D.play("fall")
+	
+	if left_ray.is_colliding() or right_ray.is_colliding(): # Check for if anything is to the left or right of the player and has a collider
+		max_jumps = 1 # Sets jump max to 1
+		player_speed = move_speed * .70 # Lowers players speed untill they touch the floor again
+		velocity = Vector2.ZERO # Stops player's momentum when colliding with a wall
+		velocity.y += gravity * 0.4 # Slows gravity's effect on the player when on a wall
+		
+	#-- Check for if the player has jump from a left wall to a right(or vice-versa) used to preventing cheesing the wall jump--#
+	if left_ray.is_colliding() and !did_right_jump:
+		jump_count = 0
+		did_right_jump = true
+		did_left_jump = false
+	if right_ray.is_colliding() and !did_left_jump:
+		jump_count = 0
+		did_left_jump = true
+		did_right_jump = false
+	#--------------------------------------------------------------------------------------------------------------------------#
+	
+	if is_on_floor():
+		did_left_jump = false # Resets the wall jump checker
+		did_right_jump = false # Resets the wall jump checker
+		max_jumps = 2 # Sets jump max to 2
+		player_speed = move_speed #Reset player speed to base speed on floor
+		jump_count = 0 # Resets Amount of times jumped
+		
+	#-- Check for if player tried to dash and can dash --#
+	if Input.is_action_just_pressed("dash") and can_dash: 
+		is_dashing = true # sStates player is dashing
+		can_dash = false # States they can't dash
+		$dash_timer.start() # Starts timer for the player's dash
+		$can_dash_timer.start() # Starts timer for cooldown
+	#----------------------------------------------------#
+	
+	if Input.is_key_pressed(KEY_LEFT): # Check if player pressed left key
+		if is_dashing:
+			velocity.x = direction * dash_speed # Moves player left faster
+		else: 
+			velocity.x = direction * player_speed # Moves the player left normally
+		if is_on_floor:
+			$AnimatedSprite2D.play("run")
+	elif Input.is_key_pressed(KEY_RIGHT): # Check if player pressed right key
+		if is_dashing:
+			velocity.x = direction * dash_speed # Moves player right faster
+		else:
+			velocity.x = direction * player_speed # Moves the player right normally
+		if is_on_floor:
+			$AnimatedSprite2D.play("run") # Plays run animation if on floor
+	else: 
+		velocity.x = 0 # stops player of they aren't trying to move
+	
+	if jump_count < max_jumps: #Checks if the player has jumped the max amount of time or not
+		if Input.is_action_just_pressed("ui_accept"): #Checks if space was just pressed
+			velocity.y = jump_force #Moves player up
+			jump_count+=1 #Increments jump count by 1 when you have jumped
+			$AnimatedSprite2D.play("jump") #Plays jump animation
+	
+	if Input.is_action_just_pressed("plant_bridge"):
+		state = !state # Flips state to either true or false
+		bridges[index].visible = state #Makes the specific bridge in the array be either visible or !visible
+
+	if global_position.y > bottom_bound: #Game over if below bottom bound
+		game_over()
+		
+	move_and_slide()
+
 func game_over(): # Reloads scene when death
 	get_tree().reload_current_scene()
 	
@@ -51,78 +124,10 @@ func move_animation(): # Hold animations for the movement
 		$AnimatedSprite2D.play("idle")
 	if direction != 0 and not is_on_floor():
 		$AnimatedSprite2D.play("fall")
-		
-
-func movement_control(delta): # Holds all movement control
-	var direction = Input.get_axis("left", "right")
-	if (not is_on_floor() and not is_on_wall()): # Applies gravity if not in floor and not on a wall
-		velocity.y += gravity
-		$AnimatedSprite2D.play("fall")
 	
-	if left_ray.is_colliding() or right_ray.is_colliding():
-		max_jumps = 1 # Sets jump max to 1
-		player_speed = move_speed * .70 # Lowers players speed untill they touch the floor again
-		velocity = Vector2.ZERO # Stops player's momentum when colliding with a wall
-		velocity.y += gravity * 0.4 # Slows gravity's effect on the player when on a wall
-	if left_ray.is_colliding() and !did_right_jump:
-		jump_count = 0
-		did_right_jump = true
-		did_left_jump = false
-	if right_ray.is_colliding() and !did_left_jump:
-		jump_count = 0
-		did_left_jump = true
-		did_right_jump = false
-			
-	if is_on_floor():
-		did_left_jump = false
-		did_right_jump = false
-		max_jumps = 2 # Sets jump max to 2
-		player_speed = move_speed #Reset player speed to base speed on floor
-		jump_count = 0 # Resets Amount of times jumped
-	
-	if Input.is_action_just_pressed("dash") and can_dash:
-		is_dashing = true
-		can_dash = false
-		$dash_timer.start()
-		$can_dash_timer.start()
-	
-	if Input.is_key_pressed(KEY_LEFT): # Move left aslong as they didn't just jump right off a wall
-		if is_dashing:
-			velocity.x = direction * dash_speed
-		else: 
-			velocity.x = direction * player_speed # Moves the player left
-		if is_on_floor:
-			$AnimatedSprite2D.play("run")
-	elif Input.is_key_pressed(KEY_RIGHT): # Move right aslong as they didn't just jump left off a wall
-		if is_dashing:
-			velocity.x = direction * dash_speed
-
-		else:
-			velocity.x = direction * player_speed # Moves the player right
-		if is_on_floor:
-			$AnimatedSprite2D.play("run")
-	else: 
-		velocity.x = 0
-	
-	if jump_count < max_jumps: #Checks if the player has jumped the max amount of time or not
-		if Input.is_action_just_pressed("ui_accept"): #Checks if space was just pressed and not on a wall.
-			velocity.y = jump_force #Moves player up
-			jump_count+=1 #Increments jump count by 1 when you have jumped
-			$AnimatedSprite2D.play("jump")
-	
-	if Input.is_action_just_pressed("plant_bridge"):
-		state = !state
-		bridges[index].visible = state
-		
-		
-	if global_position.y > bottom_bound: #Game over if below bottom bound
-		game_over()
-		
-	move_and_slide()
-
-func _on_dash_timer_timeout():
+func _on_dash_timer_timeout(): #Stops player from dashing after the timer is up
 	is_dashing = false;
 
-func _on_can_dash_timer_timeout():
+func _on_can_dash_timer_timeout(): # Allows the player to dash after the cooldown is up
 	can_dash = true
 	
