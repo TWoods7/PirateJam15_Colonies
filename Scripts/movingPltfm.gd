5ab936394683f5b3dacb10e49a47e5fd531552b3
akelwalk <104277293+akelwalk@users.noreply.github.com>
2024-07-23 14:21:29 -0700

    test

---
+++ Scripts/movingPltfm.gd
@@ -0,0 +1,14 @@
+extends Path2D
+
+@export var open_path = true #open path is a line, a closed path is a shape, like a circle
+@export var speed = 2
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#if open_path:
+		#$AnimationPlayer
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
