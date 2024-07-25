401e36c036c3012441d0959c5f2e158f51ae744f
akelwalk <104277293+akelwalk@users.noreply.github.com>
2024-07-25 16:41:58 -0700

    options menu progress

--- Scripts/menu_scripts/start_button.gd
+++ Scripts/menu_scripts/start_button.gd
@@ -2,9 +2,7 @@ extends Button
 
 
 func _on_pressed():
-	print("start pressed")
 	get_tree().change_scene_to_file("res://Scenes/Forest_Level.tscn")
-
 
 func _on_mouse_entered():
 	$"../Hover".play()
