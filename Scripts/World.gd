fe2eeef6abdfa9d443bd3fdef89de9c0ce8eaf04
akelwalk <104277293+akelwalk@users.noreply.github.com>
2024-07-25 13:52:49 -0700

    checkpoints

--- Scripts/World.gd
+++ Scripts/World.gd
@@ -2,23 +2,16 @@ extends Node2D
 @onready var player = $Player
 @onready var bottom_bound = 100
 @onready var starting_spawn = $Starting_Spawn
-var starting_position = Vector2(77, -85)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if Checkpoints.spawn:
-		player.global_position = Checkpoints.spawn
-	else:
-		player.global_position = starting_spawn.global_position
+	player.global_position = starting_spawn.global_position
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#print(player.global_position)
 	if player.global_position.y > bottom_bound: #Game over if below bottom bound
-		
 		game_over()
 	
 func game_over(): # Teleports player to start or a checkpoint
-	#player.position = starting_position
 	print("game over")
 	if Checkpoints.spawn:
 		player.global_position = Checkpoints.spawn
