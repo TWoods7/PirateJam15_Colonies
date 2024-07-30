9cd9c2eed6acde1c19359bb7b1b2e1a483feda26
Taylor Woods <taywoods6284@gmail.com>
2024-07-30 18:31:31 -0400

    Re-added spikes

--- Scripts/Bridge_Node.gd
+++ Scripts/Bridge_Node.gd
@@ -3,8 +3,14 @@ extends Area2D
 @export var is_on_node = false
 
 func _on_body_entered(body):
-	is_on_node = true
+	if body.is_in_group("Clive"):
+		is_on_node = true
+	else:
+		pass
 
 
 func _on_body_exited(body):
-	is_on_node = false
+	if body.is_in_group("Clive"):
+		is_on_node = false
+	else:
+		pass
