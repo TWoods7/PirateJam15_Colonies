f6a9d11e270a4b4304651fb95f9d73983b3b4b23
Taylor Woods <taywoods6284@gmail.com>
2024-07-30 11:40:41 -0400

    teleport to the right level

--- Scripts/menu_scripts/skip_button.gd
+++ Scripts/menu_scripts/skip_button.gd
@@ -1,7 +1,7 @@
 extends Button
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://Scenes/levels/level1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Level1.tscn")
 
 
 func _on_mouse_entered():
